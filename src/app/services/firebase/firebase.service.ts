33d33921dbf0e00f6aad794291fe750d0ae497aa
abrahmanzai <abharahmanzai25@gmail.com>
2025-06-26 14:37:11 -0500

    first commit

---
+++ src/app/services/firebase/firebase.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { collection, collectionData, CollectionReference, deleteDoc, doc, DocumentReference, Firestore, getDocs, setDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(private firestore: Firestore) { }
+
+
+  async creatDoc(targetCollection: string, docId: string, name: string, score: number) {
+    let docRef: DocumentReference = doc(this.firestore, targetCollection, docId)
+    await setDoc(docRef, { name: name, score: score })
+
+  }
+  readDocs(targetCollection: string): Observable<any> {
+    let collectionRefrence: CollectionReference = collection(this.firestore, targetCollection)
+    return collectionData(collectionRefrence)
+
+
+  }
+
+  async updateDoc(targetCollection: string, docId: string, name: string, score: number) {
+    let docRef: DocumentReference = doc(this.firestore, targetCollection, docId)
+    await setDoc(docRef, { name: name, score: score })
+
+  }
+  async deleteDocument(targetCollection: string, docId: string) {
+    let docRef: DocumentReference = doc(this.firestore, targetCollection, docId)
+    await deleteDoc(docRef)
+
+  }
+}
+
