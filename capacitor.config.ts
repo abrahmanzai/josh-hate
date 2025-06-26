25944c951d584c38e15c451ec817d019b760961c
abrahmanzai <abharahmanzai25@gmail.com>
2025-06-26 14:10:57 -0500

    add files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'josh_is_inLoveWithAtilla',
+  webDir: 'www'
+};
+
+export default config;
